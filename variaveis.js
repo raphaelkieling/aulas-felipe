bb2c627b37b73f16e8e7d5d59cf7455ca5abd919
= <rapha_kieling@hotmail.com>
2020-07-18 11:16:45 -0300

    Falando sobre as variáveis const, let e var

--- variaveis.js
+++ variaveis.js
@@ -4,5 +4,15 @@
  * -- Hoisting
  * -- Function é uma variável?
  */
+const procuraCep = {
+  ativo: true,
+};
+let cep = "";
+let cepName = "";
 
-const variable = 2;
+if (procuraCep.ativo) {
+  cep = "955555-0000";
+  cepName = "Capão da canoa";
+}
+
+console.log(cep, cepName);
