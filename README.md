a93bcd7f12f00fa606ddde2296e49d58f30f2aea
= <rapha_kieling@hotmail.com>
2020-07-18 12:08:58 -0300

    Adicionado conceitos de callback, arquivo de legenda e passando algumas coisas pelo readme.md

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Aulas felipe
 
 Esse repositório foi criado para as aulas com o felipe.
+
+### Primeira aula
+
+### Desafio inicial
+
+- Criar um repositório
+- Dar os comandos iniciais do git em uma pasta do computador
+  - `git init`
+  - `git add (. ou -A)`
+  - `git commit -m "..."`
+  - Na primeira vez tu tem que dar `git add remote caneca <<link do repositorio>>`
+  - `git push caneca master`
--- callback.js
+++ callback.js
@@ -1,7 +1,37 @@
 /**
  * Callbacks e callback hell
+ * -- Arrow functions
+ * -- Funções anônimas
  * -- Se funções são variáveis...
  * -- E se eu passar uma dentro de outra, dentro de outra?
- * -- Funções anônimas
- * -- Arrow functions
  */
+
+// function pegaNomeDoBancoDeDados(callbackNome) {
+//   callbackNome("Diego");
+// }
+
+// pegaNomeDoBancoDeDados(function (nome) {
+//   console.log("Peguei do banco de dados o nome: " + nome);
+// });
+
+// fs.readFile("./legendas-filme.txt", function (erro, buffer) {
+//   if (!erro) console.log(String(buffer));
+// });
+
+const leitor = {
+  lerArquivo: function (pathDoArquivo, cachorroAzul) {
+    // Finge que estou lendo o arquivo de verdade
+    const conteudo = fs.readFileSync(pathDoArquivo);
+    setTimeout(function () {
+      cachorroAzul(null, String(conteudo));
+    }, 5000);
+  },
+};
+
+function mostraOArquivoQueFoiLido(erroQueAconteceu, conteudo) {
+  console.log(erroQueAconteceu);
+  console.log(conteudo);
+}
+
+leitor.lerArquivo("./legendas-filme.txt", mostraOArquivoQueFoiLido);
+console.log("Esta acontecendo durante a leitura do arquivo");
